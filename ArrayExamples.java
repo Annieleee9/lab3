b2154f3a9989759a76ff40a551e5dea40f10a960
Annieleee9 <jol104@ucsd.edu>
2023-10-23 16:55:39 -0700

    update arraytest

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,10 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int temp = arr[0];
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length-i-1]=temp;
     }
   }
 
@@ -16,7 +18,7 @@ public class ArrayExamples {
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = newArray[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
